d8bd6f737d5cf1864563ca4c2bef60cf5d9cd394
Martinlindahltest <basmartin@gmail.com>
2020-01-20 12:27:29 +0100

    speed typing game 8 är färdig

--- src/App.js
+++ src/App.js
@@ -1,65 +1,13 @@
-/**
- * Challenge: build the basic structure of our game
- * 
- * 1. <h1> title at the top
- * 2. <textarea> for the box to type in 
- *      (tip: React normalizes <textarea /> to be more like <input />, 
- *      so it can be used as a self-closing element and uses the `value` property
- *      to set its contents)
- * 3. <h4> ti display the amount of time remaining
- * 4. <button> to start the game
- * 5. Another <h1> to display the word count
- */
+import React from 'react';
 
-
-import React, { useState, useEffect, useRef } from 'react';
+import MartinsHook from './Hooks/martinsHook';
 
 function App() {
 
-  const STARTING_TIME = 5
+  const { inputRef, textarea, isTimeRunning, setTextarea, timeRemaining, handleButtonClick, antalOrd } = MartinsHook()
 
 
-  const [textarea, setTextarea] = useState('placeholder')
-  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-  const [isTimeRunning, setisTimeRunning] = useState(false)
-  const inputRef = useRef(null)
 
-  function calculateWordCount(input) {
-    const wordsArr = input.trim().split(" ")
-    const filteredWords = wordsArr.filter(word => word !== "")
-    return filteredWords.length
-  }
-
-  let antalOrd = '???'
-
-  useEffect(() => {
-    if (timeRemaining > 0 && isTimeRunning) {
-      setTimeout(() => {
-        setTimeRemaining(timeRemaining - 1)
-      }, 1000);
-    }
-
-    if (timeRemaining === 0) {
-      setTimeRemaining(STARTING_TIME)
-
-    }
-
-  }, [timeRemaining, isTimeRunning]);
-
-  if (timeRemaining === 0 && isTimeRunning === true) {
-    setisTimeRunning(false)
-  }
-
-  if (isTimeRunning === false) {
-    antalOrd = calculateWordCount(textarea)
-  }
-
-  function handleButtonClick() {
-    inputRef.current.disabled = false
-    inputRef.current.focus()
-    setTextarea('')
-    setisTimeRunning(true)
-  }
 
   return (
     <div className="App">
--- src/Hooks/martinsHook.js
+++ src/Hooks/martinsHook.js
@@ -1,9 +1,54 @@
 import { useState, useEffect, useRef } from "react"
 
 
-export default function martinsHook() {
+export default function MartinsHook() {
 
-    return
+    const STARTING_TIME = 5
+
+
+    const [textarea, setTextarea] = useState('placeholder')
+    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+    const [isTimeRunning, setisTimeRunning] = useState(false)
+    const inputRef = useRef(null)
+
+    function calculateWordCount(input) {
+        const wordsArr = input.trim().split(" ")
+        const filteredWords = wordsArr.filter(word => word !== "")
+        return filteredWords.length
+    }
+
+    let antalOrd = '???'
+
+    useEffect(() => {
+        if (timeRemaining > 0 && isTimeRunning) {
+            setTimeout(() => {
+                setTimeRemaining(timeRemaining - 1)
+            }, 1000);
+        }
+
+        if (timeRemaining === 0) {
+            setTimeRemaining(STARTING_TIME)
+
+        }
+
+    }, [timeRemaining, isTimeRunning]);
+
+    if (timeRemaining === 0 && isTimeRunning === true) {
+        setisTimeRunning(false)
+    }
+
+    if (isTimeRunning === false) {
+        antalOrd = calculateWordCount(textarea)
+    }
+
+    function handleButtonClick() {
+        inputRef.current.disabled = false
+        inputRef.current.focus()
+        setTextarea('')
+        setisTimeRunning(true)
+    }
+
+    return { inputRef, textarea, isTimeRunning, setTextarea, timeRemaining, handleButtonClick, antalOrd }
 
 }
 
